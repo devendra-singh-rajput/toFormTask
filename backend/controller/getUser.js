66c36efa0da1399ba3f5287dc81b9f7536d5240f
devendra-singh-rajput <148437618+devendra-singh-rajput@users.noreply.github.com>
2025-07-30 13:20:24 +0530

    Update getUser.js

--- backend/controller/getUser.js
+++ backend/controller/getUser.js
@@ -2,7 +2,8 @@ const User = require("../model/user");
 
 const getUser =async (req,res) => {
     try {
-        const users = await User.find();
+        const users = "user get success";
+        // await User.find();
         res.status(200).json(users);
       } catch (error) {
         res.status(400).json({ message: error.message });
